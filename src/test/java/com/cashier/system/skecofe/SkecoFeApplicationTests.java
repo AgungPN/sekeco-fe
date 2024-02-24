673dde89086ea1615044e79ad770171254ff45fa
Inno013 <innobanggo013@gmail.com>
2024-02-25 06:58:41 +0700

    first Code Commit

---
+++ src/test/java/com/cashier/system/skecofe/SkecoFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashier.system.skecofe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkecoFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
