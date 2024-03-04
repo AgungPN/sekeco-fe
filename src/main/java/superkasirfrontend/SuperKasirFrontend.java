58cce37447a241d11cb3e16875fba705e581a686
Ernest Glorius Deo <ernestgloriusdeo@gmail.com>
2024-03-04 18:42:21 +0700

    tetap sama

--- src/main/java/superkasirfrontend/SuperKasirFrontend.java
+++ src/main/java/superkasirfrontend/SuperKasirFrontend.java
@@ -73,6 +73,11 @@ public class SuperKasirFrontend extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SuperKasirFrontend().setVisible(true);
+                
+                
+                
+                
+                
             }
         });
     }
